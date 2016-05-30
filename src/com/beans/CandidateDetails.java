35b34e2460efb4ba22c403e4b14b781979cf0ebb
Sandip Dutta <mesandip.dutta@gmail.com>
2016-05-30 16:30:38 +0530

    Add files via upload

---
+++ src/com/beans/CandidateDetails.java
@@ -0,0 +1,132 @@
+package com.beans;
+
+import java.util.ArrayList;
+
+public class CandidateDetails {
+
+	private String candidateId;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String miles;
+	private String email;
+	private String address;
+	private String phoneNo;
+	private String experience;
+	private String degree;
+	private String subject;
+	private String university;
+	private String skill;
+	private String skill1;
+	private String skill2;
+	private String drivinglicence;
+	private String carowner;
+	public String getCandidateId() {
+		return candidateId;
+	}
+	public void setCandidateId(String string) {
+		this.candidateId = string;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMiles() {
+		return miles;
+	}
+	public void setMiles(String miles) {
+		this.miles = miles;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getUniversity() {
+		return university;
+	}
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+	
+	public String getSkill() {
+		return skill;
+	}
+	public void setSkill(String skill) {
+		this.skill = skill;
+	}
+	public String getSkill1() {
+		return skill1;
+	}
+	public void setSkill1(String skill1) {
+		this.skill1 = skill1;
+	}
+	public String getSkill2() {
+		return skill2;
+	}
+	public void setSkill2(String skill2) {
+		this.skill2 = skill2;
+	}
+	public String getDrivinglicence() {
+		return drivinglicence;
+	}
+	public void setDrivinglicence(String drivinglicence) {
+		this.drivinglicence = drivinglicence;
+	}
+	public String getCarowner() {
+		return carowner;
+	}
+	public void setCarowner(String carowner) {
+		this.carowner = carowner;
+	}   
+	
+	
+	
+	
+	
+}
---
+++ src/com/beans/JobProfile.java
@@ -0,0 +1,151 @@
+package com.beans;
+
+public class JobProfile {
+	
+	private String jobid;
+	private String jobTitle;
+	private String jobTime;
+	private String location;
+	private String Salary;
+	private String miles;
+	private String jobType;
+	private String jobExp;
+	private String degree;
+	private String subject;
+	private String institution;
+	private String drivingLicence;
+	private String carowner;
+	
+	
+	public JobProfile() {
+		super();
+	}
+	
+	public String getJobid() {
+		return jobid;
+	}
+
+	public void setJobid(String string) {
+		this.jobid = string;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+
+	public String getJobTime() {
+		return jobTime;
+	}
+
+
+	public void setJobTime(String jobTime) {
+		this.jobTime = jobTime;
+	}
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+	public String getSalary() {
+		return Salary;
+	}
+
+
+	public void setSalary(String salary) {
+		Salary = salary;
+	}
+
+
+	public String getMiles() {
+		return miles;
+	}
+
+
+	public void setMiles(String miles) {
+		this.miles = miles;
+	}
+
+
+	public String getJobType() {
+		return jobType;
+	}
+
+
+	public void setJobType(String jobType) {
+		this.jobType = jobType;
+	}
+
+
+	public String getJobExp() {
+		return jobExp;
+	}
+
+
+	public void setJobExp(String jobExp) {
+		this.jobExp = jobExp;
+	}
+
+
+	public String getDegree() {
+		return degree;
+	}
+
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+
+	public String getSubject() {
+		return subject;
+	}
+
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+
+	public String getInstitution() {
+		return institution;
+	}
+
+
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+
+
+	public String getDrivingLicence() {
+		return drivingLicence;
+	}
+
+
+	public void setDrivingLicence(String drivingLicence) {
+		this.drivingLicence = drivingLicence;
+	}
+
+
+	public String getCarowner() {
+		return carowner;
+	}
+
+
+	public void setCarowner(String carowner) {
+		this.carowner = carowner;
+	}
+}
+	
